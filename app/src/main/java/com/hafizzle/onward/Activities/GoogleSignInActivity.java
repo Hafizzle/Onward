a19588077bff6ebf719c51423a702c202989e789
Hafizzle <34069775+Hafizzle@users.noreply.github.com>
2018-12-17 19:18:13 -0600

    Add HomeActivity
    Add HomeActivity, update UpdateUI Method to change activities.

--- app/src/main/java/com/hafizzle/onward/Activities/GoogleSignInActivity.java
+++ app/src/main/java/com/hafizzle/onward/Activities/GoogleSignInActivity.java
@@ -174,16 +174,20 @@ public class GoogleSignInActivity extends BaseActivity implements
     private void updateUI(FirebaseUser user) {
         hideProgressDialog();
         if (user != null) {
-            mStatusTextView.setText(getString(R.string.google_status_fmt, user.getEmail()));
-            mDetailTextView.setText(getString(R.string.firebase_status_fmt, user.getUid()));
 
-            findViewById(R.id.signInButton).setVisibility(View.GONE);
-            findViewById(R.id.signOutAndDisconnect).setVisibility(View.VISIBLE);
+
+            startActivity(new Intent(this, HomeActivity.class));
+
+            //mStatusTextView.setText(getString(R.string.google_status_fmt, user.getEmail()));
+            //mDetailTextView.setText(getString(R.string.firebase_status_fmt, user.getUid()));
+
+            //findViewById(R.id.signInButton).setVisibility(View.GONE);
+            //findViewById(R.id.signOutAndDisconnect).setVisibility(View.VISIBLE);
         } else {
             mStatusTextView.setText(R.string.signed_out);
             mDetailTextView.setText(null);
 
-            Toast.makeText(this, "Sign in failed", Toast.LENGTH_SHORT).show();
+            //Toast.makeText(this, "Sign in failed", Toast.LENGTH_SHORT).show();
 
             findViewById(R.id.signInButton).setVisibility(View.VISIBLE);
             findViewById(R.id.signOutAndDisconnect).setVisibility(View.GONE);
---
+++ app/src/main/java/com/hafizzle/onward/Activities/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.hafizzle.onward.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.hafizzle.onward.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
